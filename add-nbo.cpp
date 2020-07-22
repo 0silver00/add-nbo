4f877ced4923236a384c8f47a138a2fcaae56ab4
0silver00 <dmsdudskql@gmail.com>
2020-07-22 15:17:16 +0900

    Add add-nbo

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <netinet/in.h>
-
+ 
 /*
 uint32_t add-nbo(uint32_t *n){
 	return (n & 0xFF000000) >> 24 | (n & 0x00FF0000) >> 8 | (n & 0x0000FF00) << 8 | (n & 0x000000FF) <<24;
